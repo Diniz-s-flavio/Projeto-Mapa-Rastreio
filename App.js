340fd9db90742fbef68426fb1f65cb4dc2538404
Flávio Diniz de Sousa <dinizdesousaflavio@gmail.com>
2022-08-30 10:25:46 -0300

    :checkered_flag: - F esse projeto(por enquanto)
    Durante a faze de a codificação da parte de busca de endereços. ate então não consegui resolver portanto vou dar um tempo. desse projeto para não trava meu aprendizado em React Native

--- App.js
+++ App.js
@@ -3,30 +3,73 @@ import { StyleSheet, Text, View } from 'react-native';
 import {CSS} from './assets/CSS/Css.js'
 import MapView from 'react-native-maps';
 import * as Location from 'expo-location';
-import * as Permissions from 'expo-permissions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import config from'./config/index.json';
 
 export default function App() {
 
-  const [origin,setOrigin]=useState(null)
+  const [origin,setOrigin]=useState(null);
+  const [destination,setDestination]=useState(null);
+
+ useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status === 'granted') {
+        let location = await Location.getCurrentPositionAsync({enableHighAccuracy: true});
+        setOrigin({latitude:location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta:0.00922,
+          longitudeDelta:0.00421})
+      }else{
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+    })();
+  }, []);
+  
+//Google Places Autocomplete
+const GooglePlacesInput = () => {
+  return (
+    <GooglePlacesAutocomplete
+      placeholder='Para onde vamos?'
+      onPress={(data, details = null) => {
+        // 'details' is provided when fetchDetails = true
+        console.log(data, details);
+      }}
+      query={{
+        key: config.googleApi,
+        language: 'pt-br',
+      }}
+      enablePoweredByContainer={false}
+      fetchDetails={true}
+      styles={{listView:{
+        height:20,
+        position: "absolute",
+        top: 40,
+        borderColor: "gray",
+        borderLeftWidth: 1,
+        borderRightWidth: 1,
+        borderBottomWidth: 1,
+        borderBottomLeftRadius: 4,
+        borderBottomRightRadius: 4}}}
+    />
+  );
+};
 
 //Impreções
   return (
     <View style={CSS.container}>
       <MapView style={CSS.map}
-              initialRegion={{
-                latitude:-17.036416,
-                longitude:-48.304703,
-                latitudeDelta:0.000922,
-                longitudeDelta:0.000421,
-              }}
+              initialRegion={origin}
+              showsUserLocation={true}
+              zoomEnabled={true}
+              loadingEnabled={true}
       >
 
       </MapView>
       <View style={CSS.search}>
-        
+      <GooglePlacesInput/>
       </View>
     </View>
   );
 }
-
-
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Projeto-mapa-ratiamento
+# Projeto-mapa-ratreamento
+
  Ola, esse e um pequeno projeto que tem a intenção de cria uma ferramenta de rastreamento de entregas. A principal tecnologia utilizada é o React Native, utilizando de Expo-cli, e outras dependencias como Expo Location, Expo Permissions e o MapView do React Native Maps
+
+# Icones
+    📦 Nova Funcionalidade :packege:
+    🆙 Atualização :up:
+    🏁 Releaze :chequered_flag:
